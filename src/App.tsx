ee41d42be1bc9bb2af49b1a76268bf48a53394a4
Ahmed Youssef <ahyoussef@pm.me>
2021-03-11 17:15:23 -0800

    Run tests as pre commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ const App: React.FunctionComponent = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
